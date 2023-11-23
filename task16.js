5527c7ed01f6b4bc47c92a1b1e8950dec9293bdf
ruchitakb7 <138369727+ruchitakb7@users.noreply.github.com>
2023-11-24 01:55:28 +0530

    Create task16.js

---
+++ task16.js
@@ -0,0 +1,83 @@
+console.log("person1 shows ticket");
+      console.log("person2 shows ticket");
+      const preMovie = async () => {
+        const person3PromiseToShowTicketWhenWifeArrives = new Promise(
+          (resolve, reject) => {
+            setTimeout(() => resolve("ticket"), 3000);
+          }
+        );
+        const getPopcorn = new Promise((resolve, reject) => {
+          setTimeout(() => resolve("popcorn"), 3000);
+        });
+
+        const addButter = new Promise((resolve, reject) => {
+          setTimeout(() => resolve("butter"), 3000);
+        });
+
+        const getColdDrinks = new Promise((resolve, reject) => {
+          setTimeout(() => resolve("cold-drinks"), 3000);
+        });
+
+          
+       let ticket = await person3PromiseToShowTicketWhenWifeArrives;
+       let[popcorn,butter,drink] =await Promise.all([ getPopcorn,addButter,getColdDrinks])
+       console.log(popcorn,butter,drink);
+       
+        return ticket;
+      };
+
+
+const array = [];
+    
+      function createpost(post1) {
+        return new Promise(async (resolve, reject) => {
+          array.push(post1);
+         
+          resolve(post1);
+        });
+      }
+
+      function showpost() {
+    
+        array.forEach((element) => {
+          console.log(element);
+        });
+      }
+
+      function deleteBlog() {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (array.length > 0) {
+              const last = array.pop();
+              resolve(last);
+            } else {
+              reject("ERROR");
+            }
+          }, 1000);
+        });
+      }
+
+      async function output() {
+        try {
+          await Promise.all([
+            createpost("post1"),
+            createpost("post2"),
+            showpost(),
+          ]);
+         await deleteBlog(); 
+          await showpost();
+          await deleteBlog(); 
+            await deleteBlog(); 
+           await showpost();
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      output();
+
+
+      preMovie().then((t) => console.log(`person4 shows ${t}`));
+
+      console.log("person4 shows ticket");
+   

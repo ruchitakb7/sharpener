d96245e93a3e81a6a6a41dc07d29f07f2f2c5ab2
ruchitakb7 <138369727+ruchitakb7@users.noreply.github.com>
2023-11-24 01:36:17 +0530

    Add files via upload

---
+++ promiseexe.js
@@ -0,0 +1,71 @@
+let userActive = [];
+
+let user = {
+
+ userName: "abc",
+
+ lastActivetime: 0
+
+}
+
+function updateLasteUseractivityTime() {
+
+ return new Promise((resolve, reject) => {
+
+  setTimeout(() => {
+
+   user.lastActivetime = new Date().getTime();
+
+   resolve(user.lastActivetime);
+
+  }, 1000);
+
+ });
+
+}
+
+function creatpost(value) {
+
+ return new Promise((resolve, reject) => {
+
+  setTimeout(() => {
+
+   user.userName = value;
+
+   resolve(user.userName);
+
+  });
+
+ });
+
+}
+
+function deletePost() {
+
+ return new Promise((res, rej) => {
+
+  setTimeout(() => {
+
+   userActive.pop();
+
+   res();
+
+  }, 1000);
+
+ });
+
+}
+
+Promise.all([updateLasteUseractivityTime(), creatpost("ruchita")]).then(
+
+ (msg) => {
+
+  // printDetails();
+
+  userActive.push(user);
+
+  console.log(msg);
+
+ }
+
+);
